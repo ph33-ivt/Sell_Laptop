ad11eebc7a1e87c33af4e81bcdf79e33f31aa29b
hoangnguyen <nguyennhdn@gmail.com>
2019-09-03 21:41:47 +0700

    add

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -11,16 +11,13 @@ class IndexController extends Controller
 {
     public function index()
     {
-        if(Auth::check() && Auth::user()->can('isAdmin',5))
-        {
-            return redirect()->route('admin.dashboard');
-        }else{
+
            // $products = Product::where('category_id',1)->take(2)->orderBy('id','desc')->get();
            $productsApple = Product::where('category_id',1)->get();
             $topProducts =  \DB::table('products')->where('category_id','<>',1)->inRandomOrder()->limit(8)->get();
         //  $topProducts = Product::where('category_id',7)->get();
            //Product::where('category_id','<>',1)->limit(6)->pluck('name');
             return view('frontend.index',compact('productsApple','topProducts'));
-        }
+
     }
 }
